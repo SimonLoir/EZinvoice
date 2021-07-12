5f7be6f1b5d148b07fa6f5b52d80b7a8e63a100b
Simon LOIR <simon.loir@student.unamur.be>
2021-07-12 12:14:47 +0200

    first workding version of the invoice builder

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import express from 'express';
+const puppeteer = require('puppeteer');
+const app = express();
+const port = 8080;
+
+const serialize = function (obj: any) {
+    var str = [];
+    for (var p in obj)
+        if (obj.hasOwnProperty(p)) {
+            str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+        }
+    return str.join('&');
+};
+
+async function run() {
+    const browser = await puppeteer.launch();
+
+    console.log('Pupeteer launched');
+
+    app.set('view engine', 'ejs');
+
+    app.get('/', function (req, res) {
+        let { lang } = req.query;
+
+        // Only allow a few languages
+        if (['fr'].indexOf(<string>lang) < 0)
+            return res.end('Invalid language used in the query');
+
+        res.render('pages/index', { lang });
+    });
+
+    app.get('/build', async function (req, res) {
+        const page = await browser.newPage();
+
+        await page.goto(`http://localhost:${port}/?${serialize(req.query)}`, {
+            waitUntil: 'networkidle2',
+        });
+
+        await page.pdf({
+            path: './pdf/out.pdf',
+            preferCSSPageSize: true,
+            printBackground: true,
+        });
+
+        res.end('done');
+    });
+
+    app.listen(port);
+    console.log(port + ' is the magic port');
+}
+
+run();
